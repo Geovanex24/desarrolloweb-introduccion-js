548d2bc6140647a1a027a4f51632013ee48427e2
geovanex24 <geovanex24@hotmail.com>
2023-09-16 15:26:24 -0600

    Strings o cadenas de texto

---
+++ js/04.js
@@ -0,0 +1,13 @@
+// 01- Strings o cadenas de texto
+
+// Existen 2 formas de crear Strings
+
+// 1. Más común
+const producto = "Monitor de 20 pulgadas";
+
+// 2. Menos común
+const producto2 = String("Monitor de 20 pulgadas");
+const producto3 = new String("Monitor de 20 pulgadas");
+
+console.log(producto);
+console.log(producto2);
